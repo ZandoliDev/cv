1a400a1da987fb37d28cf7c0a2d1515e6e7abfba
didier.erin <didier.erin@erintek.com>
2024-03-25 01:40:30 +0100

    add work experiences

--- src/ExperienceEtFormation.tsx
+++ src/ExperienceEtFormation.tsx
@@ -1,3 +1,4 @@
+import { Experiences } from "./Experiences"
 import { Formations } from "./Formations"
 
 export const ExperienceEtFormation = () => {
@@ -6,9 +7,7 @@ export const ExperienceEtFormation = () => {
     <section>
       <div className="grid grid-cols-1 sm:grid-cols-4 gap-4 mt-5">
         <Formations />
-        <div className="col-span-3 bg-orange-300 rounded p-2">
-          <h3 className="bg-cyan-100 text-lg text-center">Expériences</h3>
-        </div>
+        <Experiences />
       </div>
     </section>
   )
---
+++ src/Experiences.tsx
@@ -0,0 +1,80 @@
+type ExperienceType = {
+  poste: string
+  societe: string
+  statut: string
+  periode: string
+  contextes: string[]
+  activites: string[]
+  competencesTechniques: string[]
+  competencesInterpersonnelles: string[]
+}
+
+const mesExperiences: ExperienceType[] = [
+  {
+    poste: "Tech Lead Java",
+    societe: "Informatique CDC",
+    statut: "Freelance, prestataire du Groupe Onepoint",
+    periode: "depuis janvier 2022",
+    contextes: [
+      "Assistance technique",
+      "Domaine : Retraites",
+      "Contexte : MCO dans un SI d’une centaine d’applications dans une infrastructure en transformation constante",
+    ],
+    activites: [
+      "Développement de l'application de gestion de la relation client",
+      "Développement de l'application de gestion de projet",
+    ],
+    competencesTechniques: ["Java", "Spring Boot", "Angular", "PostgreSQL", "Docker", "Git"],
+    competencesInterpersonnelles: ["Java", "Spring Boot", "Angular", "PostgreSQL", "Docker", "Git"],
+  },
+]
+
+const Experience = (experience: ExperienceType) => {
+  return (
+    <>
+      <div className="border-b-2 pb-4 border-gray-200">
+        <p className="text-gray-700 font-semibold">
+          {experience.poste} - {experience.societe}
+        </p>
+        <p className="text-sm text-gray-500">{experience.statut}</p>
+        <p className="text-sm text-gray-600">{experience.periode}</p>
+
+        {experience.contextes.map((contexte) => (
+          <p className="mt-2 text-gray-600">{contexte}</p>
+        ))}
+        <p className="mt-2 text-gray-600">
+          Tâches et activités :
+          <ul className="list-disc list-inside">
+            {experience.activites.map((activite) => (
+              <li>{activite}</li>
+            ))}
+          </ul>
+        </p>
+        <p className="mt-2 text-gray-600">
+          Compétences techniques :
+          <span className="font-semibold"> {experience.competencesTechniques.join(", ")}</span>
+        </p>
+        <p className="mt-2 text-gray-600">
+          Compétences interpersonnelles :
+          <span className="font-semibold">
+            {" "}
+            {experience.competencesInterpersonnelles.join(", ")}
+          </span>
+        </p>
+      </div>
+    </>
+  )
+}
+
+export const Experiences = () => {
+  return (
+    <>
+      <div className="col-span-3 rounded p-2">
+        <h3 className="bg-cyan-100 text-lg text-center">Expériences</h3>
+        {mesExperiences.map((experience) => (
+          <Experience {...experience} />
+        ))}
+      </div>
+    </>
+  )
+}
