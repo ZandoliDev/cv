7ed982b0811327952c0b811e07bf12282d07e89a
didier.erin <didier.erin@erintek.com>
2024-03-28 14:09:24 +0100

    Introduce popup with form for CV download and engaging introduction message

--- src/Entete.tsx
+++ src/Entete.tsx
@@ -1,4 +1,12 @@
+import { useState } from "react"
+
 export const Entete = () => {
+  const [showPopup, setShowPopup] = useState(false)
+
+  const togglePopup = () => {
+    setShowPopup(!showPopup)
+  }
+
   const dateOfBirth = new Date(import.meta.env.REACT_APP_DATE_OF_BIRTH || "1970-01-01")
   const name = import.meta.env.VITE_REACT_APP_NAME
   const email = import.meta.env.VITE_REACT_APP_EMAIL
@@ -28,12 +36,12 @@ export const Entete = () => {
           <p className="mt-2">{calculateAge()} ans</p>
           <p className="mt-2">Permis B</p>
           <p className="mt-2 text-sm text-gray-600">Freelance en SASU</p>
-          <a
-            href="#"
+          <button
+            onClick={togglePopup}
             className="w-64 mt-3 inline-block bg-cyan-600 text-white text-sm font-bold rounded-full px-6 py-2"
           >
             Télécharger mon CV en PDF
-          </a>
+          </button>
         </div>
 
         <div id="titre" className="w-full ml-5 p-2 bg-cyan-200 shadow-xl rounded text-right ">
@@ -53,6 +61,66 @@ export const Entete = () => {
           </p>
         </div>
       </div>
+
+      {showPopup && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center">
+          <div className="bg-white p-5 rounded-lg max-w-lg mx-auto">
+            <div className="mb-4">
+              <p className="text-center">Je suis ravi de votre intérêt pour mon profil ! 😊</p>
+              <p className="mt-2">
+                Afin de rester en contact, je vous invite à remplir les informations ci-dessous.
+                Vous recevrez mon CV directement par email. 📧
+              </p>
+              <p className="mt-2">
+                Merci de préciser également si vous souhaitez échanger avec moi sur d'éventuelles
+                opportunités. Je me ferai un plaisir de vous contacter en retour pour organiser une
+                rencontre.
+              </p>
+              <p className="mt-2">
+                ️🛡️ Soyez assuré que vos informations demeureront confidentielles et ne seront
+                utilisées que dans le cadre de nos échanges professionnels.🛡️
+              </p>
+              <p className="mt-2">🙏 Merci pour votre compréhension et votre temps.⌛️</p>
+            </div>
+            <h3 className="text-lg font-bold mb-4">Votre société</h3>
+            <form>
+              <input
+                type="text"
+                placeholder="Nom de la société"
+                className="block w-full p-2 mb-4 border rounded"
+                required
+              />
+              <input
+                type="text"
+                placeholder="Votre poste"
+                className="block w-full p-2 mb-4 border rounded"
+                required
+              />
+              <textarea
+                placeholder="Vos missions identifiées"
+                className="block w-full p-2 mb-4 border rounded"
+                required
+              ></textarea>
+              <div className="flex justify-between">
+                <button
+                  type="button"
+                  onClick={togglePopup}
+                  className="bg-gray-500 text-white rounded px-4 py-2"
+                >
+                  Annuler
+                </button>
+                <button
+                  type="button"
+                  onClick={togglePopup}
+                  className="bg-cyan-600 text-white rounded px-4 py-2"
+                >
+                  Continuer <i className="fa-solid fa-arrow-right"></i>
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
     </header>
   )
 }
