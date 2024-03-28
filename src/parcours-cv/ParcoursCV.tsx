735c63f4af0019953ae3394e3c2279f367f40c33
didier.erin <didier.erin@erintek.com>
2024-03-28 14:20:19 +0100

    Refactor ParcoursCV component and add EtapesParcoursCV class

--- src/parcours-cv/ParcoursCV.tsx
+++ src/parcours-cv/ParcoursCV.tsx
@@ -1,24 +1,24 @@
-export const ParcoursCV = ({ togglePopup }: { togglePopup :() => void}) => {
+import { IntroductionParcoursCV } from "./IntroductionParcoursCV"
+
+class EtapesParcoursCV {
+
+  first() {
+
+    return (<IntroductionParcoursCV />)
+  }
+
+}
+
+export const ParcoursCV = ({ togglePopup }: { togglePopup: () => void }) => {
+  
+  const etapesParcoursCV = new EtapesParcoursCV()
+
+
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center">
       <div className="bg-white p-5 rounded-lg max-w-lg mx-auto">
-        <div className="mb-4">
-          <p className="text-center">Je suis ravi de votre intérêt pour mon profil ! 😊</p>
-          <p className="mt-2">
-            Afin de rester en contact, je vous invite à remplir les informations ci-dessous. Vous
-            recevrez mon CV directement par email. 📧
-          </p>
-          <p className="mt-2">
-            Merci de préciser également si vous souhaitez échanger avec moi sur d'éventuelles
-            opportunités. Je me ferai un plaisir de vous contacter en retour pour organiser une
-            rencontre.
-          </p>
-          <p className="mt-2">
-            ️🛡️ Soyez assuré que vos informations demeureront confidentielles et ne seront utilisées
-            que dans le cadre de nos échanges professionnels.🛡️
-          </p>
-          <p className="mt-2">🙏 Merci pour votre compréhension et votre temps.⌛️</p>
-        </div>
+        {etapesParcoursCV.first()}
+
         <h3 className="text-lg font-bold mb-4">Votre société</h3>
         <form>
           <input
