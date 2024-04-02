2b587c40ed7e4e4723e3f3ce6d175f243f30502e
didier.erin <didier.erin@erintek.com>
2024-04-02 23:40:38 +0200

    fix contact storing in airtable

--- src/parcours-cv/gateway/contact.gateway.ts
+++ src/parcours-cv/gateway/contact.gateway.ts
@@ -2,26 +2,52 @@ import axios from "axios"
 import { Contact } from "../model/Contact"
 
 export const sauvegarderContact = async (contact: Contact): Promise<void> => {
-  const url = `https://didier-erin.me/api/contact`
-  await axios
-    .post(url, mapContactToAirtableContact(contact), {
-      headers: {
-        // Authorization: `Bearer ${apiKey}`,
-        "Content-Type": "application/json",
-      },
-    })
-    .then(() => {
-      return Promise.resolve()
-    })
-    .catch(() => {
-      return Promise.reject()
-    })
+  const contactUrl = `https://didier-erin.me/api/contact`
+  const societetUrl = `https://didier-erin.me/api/societe`
+  try {
+    await axios
+      .post(societetUrl, mapContactToAirtableSociete(contact), {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then(async (response) => {
+        console.log(response)
+        const idSociete = response.data.id
+        await axios
+          .post(contactUrl, mapContactToAirtableContact(contact, idSociete), {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          })
+          .then(() => {
+            return Promise.resolve()
+          })
+      })
+  } catch (error) {
+    return Promise.reject()
+  }
 }
 
-const mapContactToAirtableContact = (contact: Contact) => {
+const mapContactToAirtableContact = (contact: Contact, idSociete: string) => {
   return {
     fields: {
-      contact,
+      Prenom: contact.prenom,
+      Nom: contact.nom,
+      Societe: [idSociete],
+      Poste: contact.poste,
+      Telephone: contact.telephone,
+      Email: contact.email,
+    },
+  }
+}
+
+const mapContactToAirtableSociete = (contact: Contact) => {
+  return {
+    fields: {
+      Nom: contact.societe.nom,
+      Description: contact.societe.description,
+      "Site web": contact.societe.siteWeb,
     },
   }
 }
